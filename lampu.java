894484f5fd9fe408cbbae97c90b3cf581822d441
Jefrry70 <muhammjefryy21@gmail.com>
2025-06-24 13:31:32 +0800

    Menambahkan wr wb

--- lampu.java
+++ lampu.java
@@ -1,6 +1,7 @@
 public class lampu{
+    public static void main(String[] args) {
+        System.out.println("Salam Sejahtera");
 
-    private String nama;
-    private int kelas;
+        System.out.println("Assalamualaikum wr wb");
+    }
 }
-
